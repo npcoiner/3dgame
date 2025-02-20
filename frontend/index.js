3e77cb600db5179566606cf931a901b13b11892a
npcoiner <nicholascoiner@gmail.com>
2025-02-20 15:05:30 -0800

    Render basic cube with gui camera controls

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-
+import {startGame} from "./Game/Game.js";
 const socketUrl = new URL("/", window.location.href);
 
 const socket = io(socketUrl.toString()+ "socketNamespace");
@@ -8,3 +8,14 @@ let userName = "";
 socket.on("connect", () => {
     console.log("connected to socket");
 })
+
+// document.getElementById("name-input").addEventListener("keypress", (event) => {
+//     if(event.key == 'Enter'){
+//         userName = document.getElementById("name-input").value;
+//         console.log(userName);
+//     } 
+// });
+
+
+startGame();
+console.log("game started");
